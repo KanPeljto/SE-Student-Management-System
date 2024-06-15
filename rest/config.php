60509607fced98af8bcbe8b8b5a6269c04fe74e1
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-06-15 11:49:40 +0200

    new branch

--- rest/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_NAME', 'defaultdb');
-define('DB_PORT', 25060);
-define('DB_USER', 'doadmin');
-define('DB_PASSWORD', 'AVNS_zGgnhJbrO9vhlzWtV-9');
-define('DB_HOST', 'db-mysql-fra1-17451-do-user-16822868-0.c.db.ondigitalocean.com'); 
